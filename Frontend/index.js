d5cdb899e10f7fce0d2a3e657e1b12d57705d365
Gabriel Cootauco <gp.cootauco@hotmail.co.uk>
2020-01-31 00:28:27 +0000

    fixign the index.js but having problem in CORS

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,5 +1,43 @@
-const fetchPromise = fetch("http://localhost:3000/account-info");
-fetchPromise.then(response => {
-    console.log(response);
 
-});
+
+
+
+// async function asyncCall(){
+//     const request = new Request("http://localhost:3000/account-info");
+//
+//     fetch(request, {mode: "no-cors"}).then(response =>{
+//         return response.json();
+//     }).catch(function(error) {
+//         console.log('Request failed', error)
+//     });
+//
+//
+//     // const fetchPromise = fetch("http://localhost:3000/account-info");
+//     // await fetchPromise.then(response => {
+//     //     console.log(response);
+//     // });
+// }
+//
+// asyncCall().then();
+
+async function callThis(){
+    await  fetch('http://localhost:3000/account-info')
+        .then(function(response) {
+            return response.text()
+        }).then(function(text) {
+        alert(text);
+    }).catch(function(err) {
+        console.log('Fetch problem: ' + err.message);
+    });
+}
+
+callThis();
+
+
+
+// const fetchPromise = fetch("http://localhost:3000/account-info");
+// fetchPromise.then(response => {
+//     return response.json();
+// }).then(people=> {
+//     console.log(people);
+// });
