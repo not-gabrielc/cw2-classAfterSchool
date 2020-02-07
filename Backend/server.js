4c18fa4111d85918e1517579b0890bb806885af2
Gabriel Cootauco <gp.cootauco@hotmail.co.uk>
2020-02-07 00:58:15 +0000

    was able to post data from vue to server

--- Backend/server.js
+++ Backend/server.js
@@ -1,30 +1,65 @@
+const http = require('http');
+const path = require('path');
+
 // load Express.js
 const express = require('express');
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(3000, () => console.log("Listening at 3000"));
+
+//to use cors
+const cors = require('cors');
+app.use(cors());
+
 // connect to MongoDB
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/cw2';
-var str = "";
+const str = "";
 
-//accounts and classes collection
-//CustomerId: 0,
-//CustomerEmail: "g.c@g.c",
-//CustomerPassword: "123",
-//CustomerUserType: "Admin"
 
 app.get('/', function(req, res) {
     // shows a message that API is working
     res.send('The server.js is now running. Open the index.html from WebStorm.');
 });
 
+
+//adds users to the database
+app.post('/user-add', (req, res) => {
+    console.log(req.body);
+    const data = req.body;
+    res.send({
+        status: 'success',
+        email: data.emails,
+        password: data.passwords,
+        userType: data.userTypes
+    })
+});
+
+// app.route('/user-add').post(function (req, res) {
+//     console.log(req.body);
+//     const data = req.body;
+//     res.json({
+//         status: 'success',
+//         email: data.email,
+//         password: data.password,
+//         userType: data.userType
+//     })
+// });
+
+
 app.route('/account-info').get(function(req, res) {
     MongoClient.connect(url, function(err, db) {
-
         const cursor = db.collection('accounts').find({});
-
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
 
         cursor.toArray(function(err, doc) {
             res.send(doc);
@@ -33,43 +68,58 @@ app.route('/account-info').get(function(req, res) {
     });
 });
 
-app.route('/get-user').get(function(req, res) {
-    MongoClient.connect(url, function(err, db) {
-        var collection = db.collection('accounts');
-        var cursor = collection.find({});
-        str = "";
-        cursor.forEach(function(item) {
-            if (item != null) {
-                str = str + "    Account ID:  " + item.CustomerId + "</br>" +
-                    "  Account Email: " + item.CustomerEmail + "</br>" +
-                    "  Account UserType: " + item.CustomerUserType + "</br>" +
-                    "________________________" + "</br>" + "</br>";
-            }
-        }, function(err) {
-            console.log(str)
-            res.send(str);
-            db.close();
-        });
-    });
-
-    // MongoClient.connect(url, function (err, db) {
-    //     const dat = db.collection("accounts").find();
-    //
-    //     dat.each(function(err,doc){
-    //         if (doc != null) {
-    //             str = str + "Account id" + doc.CustomerId + "</br>";
-    //         }
-    //     });
-    //     res.send(str);
-    //     db.close();
-    // });
-});
-
-app.post('/insert-user', function(req, res, next) {
-
-});
-
-
-app.listen(3000, () => {
-    console.log("Open index.html file")
-});
+// app.route('/get-user').get(function(req, res) {
+//     MongoClient.connect(url, function(err, db) {
+//         var collection = db.collection('accounts');
+//         var cursor = collection.find({});
+//         str = "";
+//         cursor.forEach(function(item) {
+//             if (item != null) {
+//                 str = str + "    Account ID:  " + item.CustomerId + "</br>" +
+//                     "  Account Email: " + item.CustomerEmail + "</br>" +
+//                     "  Account UserType: " + item.CustomerUserType + "</br>" +
+//                     "________________________" + "</br>" + "</br>";
+//             }
+//         }, function(err) {
+//             console.log(str)
+//             res.send(str);
+//             db.close();
+//         });
+//     });
+//
+//     // MongoClient.connect(url, function (err, db) {
+//     //     const dat = db.collection("accounts").find();
+//     //
+//     //     dat.each(function(err,doc){
+//     //         if (doc != null) {
+//     //             str = str + "Account id" + doc.CustomerId + "</br>";
+//     //         }
+//     //     });
+//     //     res.send(str);
+//     //     db.close();
+//     // });
+// });
+//
+// app.post('/insert-user', function(req, res, next) {
+//
+//     MongoClient.connect(url, function(err, db){
+//         if(err) throw err;
+//
+//         const user = {
+//             email: req.body.email,
+//             password: req.body.password,
+//             userType: req.body.userType
+//         };
+//
+//         MongoClient.connect(url, function (err, db) {
+//             assert.equal(err, null);
+//             db.collection('accounts').insert(user,function (err, result) {
+//                 console.log('account inserted');
+//                 db.close();
+//
+//             })
+//         })
+//
+//     })
+//
+// });
--- Frontend/index.js
+++ Frontend/index.js
@@ -1,13 +1,156 @@
-//fetch my api to the localhost.
-const fetchPromise = fetch("http://localhost:3000/account-info");
-
+// //fetch my api to the localhost.
+// const fetchPromise = fetch("http://localhost:3000/account-info");
+//
+// // fetchPromise.then(response => {
+// //     console.log(response);
+// // });
+//
+// //shows it as json format
 // fetchPromise.then(response => {
-//     console.log(response);
+//    return response.json();
 // });
 
-//shows it as json format
-fetchPromise.then(response => {
-   return response.json();
-}).then(people => {
-    console.log(people);
+
+// async function logins() {
+//     const email = document.getElementsByName('email');
+//     const password = document.getElementsByName('password');
+//     const userType = document.getElementsByName('userType');
+//
+//     const userAccount = { email, password, userType};
+//
+//     const options = {
+//         method: "POST",
+//         header: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(userAccount)
+//     };
+//     const type = await fetch('http://localhost:3000/login', options);
+//     const data = await type.json();
+//     console.log(data);
+// }
+//
+// logins();
+
+
+//---------------------------------------------- Login ------------------------------------------------------
+let userArray = [];
+const url = 'http://localhost:3000';
+
+var LoginandLogout = new Vue({
+    el: "#loginClass",
+    data: {
+        email: '',
+        password: '',
+        userType: ''
+    },
+    methods: {
+        //sign up fucntion.
+        signup: async function () {
+
+            const emails = this.email;
+            const passwords = this.password;
+            const userTypes = this.userType;
+
+            let userAccounts = {
+                emails,
+                passwords,
+                userTypes
+            };
+
+            const options = {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(userAccounts)
+            };
+
+            fetch(url+'/user-add', options).then( response=> {
+                return response.json()
+            }).then( data => {
+                console.log(data)
+            }).catch(err=>{
+                console.log(err);
+            });
+
+            // const type = await fetch(url+'/user-add', options);
+            // const data = await type.json();
+            // console.log(data);
+
+            //saves user register on the local storage
+            let userArray = '';
+            const newEmail = this.email;
+
+            if (localStorage.getItem('users')) {
+                // 'users' is an array of objects
+                userArray = JSON.parse(localStorage.getItem('users'));
+            }
+            if (userArray) {
+                if (userArray.some(function (user) {
+                    return user.email === newEmail
+                })) {
+                    alert('Email already exists!');
+                    return;
+                }
+                userArray.push({'email': newEmail, 'password': this.password, 'currentUser': this.userType});
+
+                localStorage.setItem('users', JSON.stringify(userArray));
+                alert("You are now registered!")
+            } else {
+                userArray = [{'email': newEmail, 'password': this.password, 'currentUser': this.userType}];
+                localStorage.setItem('users', JSON.stringify(userArray));
+            }
+        },
+
+        //sign in function.
+        signin: function () {
+            //saves the details of the login into a seperate array
+            const login = [{
+                'email': this.email,
+                'password': this.password,
+                'currentUser': this.userType
+            }];
+            localStorage.setItem('loginUsers', JSON.stringify(login));
+
+            //new array that will contain the parse data
+            let loginUser = '';
+            const newEmail = this.email;
+            const newPassword = this.password;
+            const newUserType = this.userType;
+
+            if (localStorage.getItem('users')) {
+                // 'users' is an array of objects
+                loginUser = JSON.parse(localStorage.getItem('users'));
+            }
+            if (loginUser) {
+                if (loginUser.some(function (user) {
+                    return user.email === newEmail
+                })) {
+
+                    if (loginUser.some(function (user) {
+                        return user.password === newPassword
+                    })) {
+                        //alerts the user if he/she is online
+                        alert("you are now logged in!")
+                    } else {
+                        alert('Error: The password is not correct.');
+                    }
+                } else {
+                    alert('Error: The email is not correct.');
+                }
+            } else {
+                userArray = [{'email': newEmail, 'password': newPassword, 'currentUser': newUserType}];
+                localStorage.setItem('users', JSON.stringify(userArray));
+            }
+        },
+
+        //sign out function.
+        signout: function () {
+            this.userType = '';
+            this.email = '';
+            this.password = '';
+        }
+    }
 });
